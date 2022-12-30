bd9221f4d83e73aacc0056474c341d13168a35a3
apple <tanyazarghani@gmail.com>
2022-12-30 16:52:05 +0330

    sudoku added

---
+++ ai.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import random
+class AI():
+  def reader():
+    filepath = input('Inter Json file path:')
+    grid = list(pd.read_json(filepath))
+    return grid
+
+  def puzzle(a):
+      for i in range(M):
+          for j in range(M):
+              print(a[i][j],end = " ")
+          print()
+  def solve(grid, row, col, num):
+      for x in range(9):
+          if grid[row][x] == num:
+              return False
+              
+      for x in range(9):
+          if grid[x][col] == num:
+              return False
+  
+  
+      startRow = row - row % 3
+      startCol = col - col % 3
+      for i in range(3):
+          for j in range(3):
+              if grid[i + startRow][j + startCol] == num:
+                  return False
+      return True
+  
+  def Suduko(grid, row, col):
+  
+      if (row == M - 1 and col == M):
+          return True
+      if col == M:
+          row += 1
+          col = 0
+      if grid[row][col] > 0:
+          return Suduko(grid, row, col + 1)
+      for num in range(1, M + 1, 1): 
+          if solve(grid, row, col, num):
+          
+              grid[row][col] = num
+              if Suduko(grid, row, col + 1):
+                  print('(', row, ',', col, ') =', num)
+                  return True
+          grid[row][col] = 0
+      return False
+AI.reader()
+AI.Suduko(grid, 0, 0)
+AI.puzzle(grid)
